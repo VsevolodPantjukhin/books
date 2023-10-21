9d31e53c6191ecc21bb0779553d2274af74e4adf
haki_ainz <seva.pantiukhin@yandex.ru>
2023-10-21 09:12:12 +0400

    State Location

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useState } from 'react';
 
 import BookCreate from '../components/BookCreate/BookCreate';
 import BookList from '../components/BookList/BookList';
 
 const App = () => {
+  const [books, setBooks] = useState([]);
+
   return (
     <div>
       <BookCreate />
