6360e5f0f9162323df3a61721168079c2e9cc5d0
haki_ainz <seva.pantiukhin@yandex.ru>
2023-10-23 01:26:00 +0400

    Adding a Book, for Real!

--- src/App/App.js
+++ src/App/App.js
@@ -8,7 +8,7 @@ const App = () => {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    console.log('Need to add book with title:', title);
+    setBooks([...books, { id: 123, title }]);
   };
 
   return (
