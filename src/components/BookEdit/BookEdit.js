911071f29bbb7ea5a6a483b8d71d5437d7f270ed
haki_ainz <seva.pantiukhin@yandex.ru>
2023-10-23 16:57:26 +0400

    Closing the Form on Submit

--- src/components/BookEdit/BookEdit.js
+++ src/components/BookEdit/BookEdit.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-const BookEdit = ({ book, onEdit }) => {
+const BookEdit = ({ book, onEdit, onSubmit }) => {
   const [title, setTitle] = useState(book.title);
 
   const handleChange = (event) => {
@@ -10,6 +10,7 @@ const BookEdit = ({ book, onEdit }) => {
   const handleSubmit = (event) => {
     event.preventDefault();
     onEdit(book.id, title);
+    onSubmit();
   };
 
   return (
--- src/components/BookShow/BookShow.js
+++ src/components/BookShow/BookShow.js
@@ -13,9 +13,13 @@ const BookShow = ({ book, onEdit, onDelete }) => {
     setShowEdit(!showEdit);
   };
 
+  const handleSubmit = () => {
+    setShowEdit(false);
+  };
+
   let content = <h3>{book.title}</h3>;
   if (showEdit) {
-    content = <BookEdit onEdit={onEdit} book={book} />;
+    content = <BookEdit onSubmit={handleSubmit} onEdit={onEdit} book={book} />;
   }
 
   return (
